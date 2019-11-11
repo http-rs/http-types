e381d71827f136dfed7c5cf9708a5773896b79d6
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-11-11 16:54:39 +0100

    asyncread for body
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/request.rs
+++ src/request.rs
@@ -1,20 +1,69 @@
+use async_std::io::{self, BufRead, Read};
+
+use std::fmt::{self, Debug};
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
 use crate::{Headers, Method, Url};
 
 /// An HTTP request.
-#[derive(Debug)]
 pub struct Request {
     method: Method,
     url: Url,
     headers: Headers,
+    body: Box<dyn BufRead + Unpin + Send + 'static>,
 }
 
 impl Request {
     /// Create a new request.
     pub fn new(method: Method, url: Url) -> Self {
+        Self::with_body(method, url, io::empty())
+    }
+
+    /// Create a new request with a body.
+    pub fn with_body(method: Method, url: Url, body: impl BufRead + Unpin + Send + 'static) -> Self {
         Self {
             method,
             url,
             headers: Headers::new(),
+            body: Box::new(body),
         }
     }
 }
+
+impl Debug for Request {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Request")
+            .field("method", &self.method)
+            .field("url", &self.url)
+            .field("headers", &self.headers)
+            .field("body", &"<hidden>")
+            .finish()
+    }
+}
+
+impl Read for Request {
+    #[allow(missing_doc_code_examples)]
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
+        Pin::new(&mut self.body).poll_read(cx, buf)
+    }
+}
+
+// TODO(yoshuawuyts): impl this
+// impl BufRead for Request {
+//     #[allow(missing_doc_code_examples)]
+//     fn poll_fill_buf(
+//         mut self: Pin<&mut Self>,
+//         cx: &mut Context<'_>
+//     ) -> Poll<io::Result<&'_ [u8]>> {
+//         self.body.poll_fill_buf(cx)
+//     }
+
+//     fn consume(mut self: Pin<&mut Self>, amt: usize) {
+//         Pin::new(&mut self.body).consume(amt)
+//     }
+// }
