8e6a1139453a082250e406fb9e7a5a4bf9ef9d4d
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-12-04 18:43:55 +0900

    init functions for header name
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/headers/header_name.rs
+++ src/headers/header_name.rs
@@ -1,6 +1,6 @@
-use std::str::FromStr;
 use std::error::Error;
 use std::fmt::{self, Display};
+use std::str::FromStr;
 
 /// A header name.
 #[derive(Debug, Eq, PartialEq, Hash)]
@@ -9,10 +9,15 @@ pub struct HeaderName {
 }
 
 impl HeaderName {
-    // /// Create a new `HeaderName`.
-    // pub fn from_ascii(name: bytes: Vec<u8>) -> Self {
-    //     Self { inner: name }
-    // }
+    /// Create a new `HeaderName`.
+    pub fn from_ascii(bytes: &[u8]) -> Result<Self, ParseError> {
+        if !bytes.is_ascii() {
+            return Err(ParseError { _private: () });
+        }
+        let string = String::from_utf8(bytes.to_ascii_lowercase())
+            .map_err(|_| ParseError { _private: () })?;
+        Ok(Self { inner: &string })
+    }
 
     /// Converts a vector of bytes to a `HeaderName` without checking that the string contains
     /// valid ASCII.
@@ -20,10 +25,12 @@ impl HeaderName {
     /// # Safety
     ///
     /// This function is unsafe because it does not check that the bytes passed to it are valid
-    /// ASCII. If this constraint is violated, it may cause memory unsafety issues with future
-    /// users of the HeaderName, as the rest of the library assumes that Strings are valid ASCII.
+    /// ASCII. If this constraint is violated, it may cause memory
+    /// unsafety issues with future users of the HeaderName, as the rest of the library assumes
+    /// that Strings are valid ASCII.
     pub const unsafe fn from_ascii_unchecked(bytes: Vec<u8>) -> Self {
-        unimplemented!();
+        let string = String::from_utf8_unchecked(bytes);
+        Self { inner: &string }
     }
 }
 
@@ -45,12 +52,14 @@ impl FromStr for HeaderName {
     type Err = ParseError;
 
     /// Create a new `HeaderName`.
-    /// 
+    ///
     /// This checks it's valid ASCII, and lowercases it.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if !s.is_ascii() {
-            return Err(ParseError { _private: () })
+            return Err(ParseError { _private: () });
         }
-        Ok(Self { inner: &s.to_ascii_lowercase() })
+        Ok(Self {
+            inner: &s.to_ascii_lowercase(),
+        })
     }
 }
