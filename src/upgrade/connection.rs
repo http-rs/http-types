cf4f77eddf7cb66c6a5c35be924c5df996a3a488
dignifiedquire <me@dignifiedquire.com>
2020-05-19 10:57:01 +0200

    get it actually to compile

--- src/upgrade/connection.rs
+++ src/upgrade/connection.rs
@@ -1,42 +1,49 @@
 use async_std::io::{self, prelude::*};
 
+use std::fmt;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
 /// An upgraded HTTP connection.
-#[derive(Debug, Clone)]
 pub struct Connection {
-    inner: Pin<Box<dyn Read + Write + Clone + Send + Sync + Unpin + 'static>>,
+    inner: Pin<Box<dyn InnerConnection + 'static>>,
 }
+
+pub(crate) trait InnerConnection: Read + Write + Send + Sync + Unpin {}
+impl<T: Read + Write + Send + Sync + Unpin> InnerConnection for T {}
 
 impl Read for Connection {
     fn poll_read(
-        self: Pin<&mut Self>,
+        mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut [u8],
     ) -> Poll<io::Result<usize>> {
-        let this = self.project();
-        Pin::new(this.inner).poll_read(cx, buf)
+        Pin::new(&mut self.inner).poll_read(cx, buf)
     }
 }
 
 impl Write for Connection {
     fn poll_write(
-        self: Pin<&mut Self>,
+        mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<io::Result<usize>> {
-        let this = self.project();
-        Pin::new(this.inner).poll_write(cx, buf)
+        Pin::new(&mut self.inner).poll_write(cx, buf)
     }
 
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        let this = self.project();
-        Pin::new(this.inner).poll_flush(cx)
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Pin::new(&mut self.inner).poll_flush(cx)
     }
 
-    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
-        let this = self.project();
-        Pin::new(this.inner).poll_close(cx)
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Pin::new(&mut self.inner).poll_close(cx)
+    }
+}
+
+impl fmt::Debug for Connection {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Connection")
+            .field("inner", &"Pin<Box<dyn Inner>>")
+            .finish()
     }
 }
