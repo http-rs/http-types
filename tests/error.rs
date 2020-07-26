be07c37205ea86471c38a022aac5baeafdf6b9bd
Jacob Rothstein <hi@jbr.me>
2020-07-25 18:55:54 -0700

    support anyhow::Error into http_types:Error

--- tests/error.rs
+++ tests/error.rs
@@ -70,3 +70,29 @@ fn option_ext() {
     let err = res.unwrap_err();
     assert_eq!(err.status(), StatusCode::NotFound);
 }
+
+#[test]
+fn anyhow_error_into_http_types_error() {
+    let anyhow_error =
+        anyhow::Error::new(std::io::Error::new(std::io::ErrorKind::Other, "irrelevant"));
+    let http_types_error: Error = anyhow_error.into();
+    assert_eq!(http_types_error.status(), StatusCode::InternalServerError);
+
+    let anyhow_error =
+        anyhow::Error::new(std::io::Error::new(std::io::ErrorKind::Other, "irrelevant"));
+    let http_types_error: Error = Error::new(StatusCode::ImATeapot, anyhow_error);
+    assert_eq!(http_types_error.status(), StatusCode::ImATeapot);
+}
+
+#[test]
+fn normal_error_into_http_types_error() {
+    let http_types_error: Error =
+        std::io::Error::new(std::io::ErrorKind::Other, "irrelevant").into();
+    assert_eq!(http_types_error.status(), StatusCode::InternalServerError);
+
+    let http_types_error = Error::new(
+        StatusCode::ImATeapot,
+        std::io::Error::new(std::io::ErrorKind::Other, "irrelevant"),
+    );
+    assert_eq!(http_types_error.status(), StatusCode::ImATeapot);
+}
