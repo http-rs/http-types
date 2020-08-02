e78336e2e74138b9ac1cbd97c6e71c06fe591289
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2020-08-02 11:11:09 +0200

    cache mod docs

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -1,4 +1,13 @@
 //! HTTP caching.
+//!
+//! Web page performance can be significantly improved by caching resources.
+//! This submodule includes headers and types to communicate how and when to
+//! cache resources.
+//!
+//! # Further Reading
+//!
+//! - [MDN: HTTP Caching](https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching)
+//! - [MDN: HTTP Conditional Requests](https://developer.mozilla.org/en-US/docs/Web/HTTP/Conditional_requests)
 
 mod cache_control;
 
