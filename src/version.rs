49b73ed2703a044e54e00b8a76f9b1f0bb0933dd
Jacob Rothstein <hi@jbr.me>
2020-09-06 17:30:05 -0700

    impl Display for Version

--- src/version.rs
+++ src/version.rs
@@ -18,9 +18,34 @@ pub enum Version {
     Http3_0,
 }
 
+impl std::fmt::Display for Version {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Version::Http0_9 => "HTTP/0.9",
+            Version::Http1_0 => "HTTP/1.0",
+            Version::Http1_1 => "HTTP/1.1",
+            Version::Http2_0 => "HTTP/2.0",
+            Version::Http3_0 => "HTTP/3.0",
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+    #[test]
+    fn to_string() {
+        let output = format!(
+            "{} {} {} {} {}",
+            Version::Http0_9,
+            Version::Http1_0,
+            Version::Http1_1,
+            Version::Http2_0,
+            Version::Http3_0
+        );
+        assert_eq!("HTTP/0.9 HTTP/1.0 HTTP/1.1 HTTP/2.0 HTTP/3.0", output);
+    }
+
     #[test]
     fn ord() {
         use Version::*;
