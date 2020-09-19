639bb3cc78852b3d9e87092324eb8823c5d67e77
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2020-09-19 18:34:15 -0400

    async_std::sync::channel -> async_channel

--- src/upgrade/sender.rs
+++ src/upgrade/sender.rs
@@ -1,21 +1,19 @@
-use async_std::sync;
-
 use crate::upgrade::Connection;
 
 /// The sending half of a channel to send an upgraded connection.
 ///
-/// Unlike `async_std::sync::channel` the `send` method on this type can only be
+/// Unlike `async_channel::Sender` the `send` method on this type can only be
 /// called once, and cannot be cloned. That's because only a single instance of
 /// `Connection` should be created.
 #[derive(Debug)]
 pub struct Sender {
-    sender: sync::Sender<Connection>,
+    sender: async_channel::Sender<Connection>,
 }
 
 impl Sender {
     /// Create a new instance of `Sender`.
     #[doc(hidden)]
-    pub fn new(sender: sync::Sender<Connection>) -> Self {
+    pub fn new(sender: async_channel::Sender<Connection>) -> Self {
         Self { sender }
     }
 
@@ -23,6 +21,6 @@ impl Sender {
     ///
     /// The channel will be consumed after having sent trailers.
     pub async fn send(self, trailers: Connection) {
-        self.sender.send(trailers).await
+        let _ = self.sender.send(trailers).await;
     }
 }
