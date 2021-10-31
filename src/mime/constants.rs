9ffd9eb9215f7947bdfb1859a7c03629f24b90be
Vic Demuzere <vic@demuzere.be>
2021-10-31 13:12:32 +0100

    Add common video, audio, archies and syndication
    References:
    
    https://github.com/nginx/nginx/blob/master/conf/mime.types
    https://www.iana.org/assignments/media-types/media-types.xhtml

--- src/mime/constants.rs
+++ src/mime/constants.rs
@@ -57,6 +57,8 @@ utf8_mime_const!(CSS, "CSS", "text", "css");
 utf8_mime_const!(HTML, "HTML", "text", "html");
 utf8_mime_const!(PLAIN, "Plain text", "text", "plain");
 utf8_mime_const!(XML, "XML", "application", "xml");
+utf8_mime_const!(RSS, "RSS Feed", "application", "rss+xml");
+utf8_mime_const!(ATOM, "Atom Feed", "application", "atom+xml");
 mime_const!(ANY, "matching anything", "*", "*");
 mime_const!(JSON, "JSON", "application", "json");
 mime_const!(SSE, "Server Sent Events", "text", "event-stream");
@@ -72,6 +74,21 @@ mime_const!(JPEG, "JPEG images", "image", "jpeg");
 mime_const!(PNG, "PNG images", "image", "png");
 mime_const!(SVG, "SVG", "image", "svg+xml");
 mime_const!(WEBP, "WebP images", "image", "webp");
+
+// Audio
+// https://www.iana.org/assignments/media-types/media-types.xhtml#audio
+mime_const!(MIDI, "MIDI audio", "audio", "midi");
+mime_const!(MP3, "MPEG audio layer 3", "audio", "mpeg");
+mime_const!(OGG, "Ogg vorbis audio", "audio", "ogg");
+mime_const!(OPUS, "Opus audio", "audio", "opus");
+mime_const!(M4A, "MPEG audio layer 4", "audio", "mp4");
+
+// Video
+// https://www.iana.org/assignments/media-types/media-types.xhtml#video
+mime_const!(MP4, "MPEG video layer 4", "video", "mp4");
+mime_const!(MPEG, "MPEG video", "video", "mpeg");
+mime_const!(WEBM, "WebM video", "video", "webm");
+mime_const!(AVI, "Microsoft AVI video", "video", "x-msvideo");
 // There are multiple `.ico` mime types known, but `image/x-icon`
 // is what most browser use. See:
 // https://en.wikipedia.org/wiki/ICO_%28file_format%29#MIME_type
@@ -83,3 +100,7 @@ mime_const!(OTF, "OTF", "font", "otf");
 mime_const!(TTF, "TTF", "font", "ttf");
 mime_const!(WOFF, "WOFF", "font", "woff");
 mime_const!(WOFF2, "WOFF2", "font", "woff2");
+
+// Archives
+mime_const!(ZIP, "Zip archive", "application", "zip");
+mime_const!(SEVENZIP, "7Zip archive", "application", "x-7z-compressed");
